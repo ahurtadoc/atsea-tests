a5d91b3dd3b1753c2235762da22078123af95548
Alexander Hurtado Cardona <ahurtadoc@unal.edu.co>
2021-12-12 17:13:32 -0500

    Initial config and simple test ui

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,7 +1,22 @@
-import { Config } from 'protractor';
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { browser, Config } from 'protractor';
+
+const chromeConfig = { browserName: 'chrome'}
+const firefoxConfig = { browserName: 'firefox', 'moz:firefoxOptions':{}}
 
 export const config: Config = {
   framework: 'mocha',
-  specs: [ '../test/google.spec.js' ],
-  seleniumAddress: 'http://localhost:4444/wd/hub'
+  specs: [ '../test/ui/*.js' ],
+  seleniumAddress: 'http://0.0.0.0:4444',
+  SELENIUM_PROMISE_MANAGER : false,
+  mochaOpts:{
+    reporter: 'mochawesome-screenshots',
+    timeout: 5000
+  },
+  multiCapabilities: [chromeConfig, firefoxConfig],
+  onPrepare: async () => {
+    browser.manage().timeouts().implicitlyWait(0);
+    await browser.waitForAngularEnabled(false);
+  }
+  
 };
---
+++ test/ui/getHealth.spec.ts
@@ -0,0 +1,19 @@
+import { browser, $ } from 'protractor';
+import { expect } from 'chai';
+import chai = require('chai');
+import chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+const url = 'http://host.docker.internal:8080'
+describe('When open main page from atsea', () => {
+  beforeEach(async () => {
+    await browser.get(url)
+  
+  });
+  it('Then give welcome message',async () => {
+    expect($('.headerTitle').getText()).to.eventually.equal('Welcome to the atsea shop');
+
+    // expect(browser.getTitle()).to.eventually.equal('Atsea Shop');
+  })
+})
